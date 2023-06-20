a363bd5cffd01e5773fc88a8eb4e35bdef96a5bd
jeongjiwooon <jjwanm@naver.com>
2023-06-20 14:18:39 +0900

    first commit

---
+++ src/main/java/com/example/study_servlet/controlls/GetRequestParamsServlet.java
@@ -0,0 +1,51 @@
+package com.example.study_servlet.controlls;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/getRequestParamsServlet")
+public class GetRequestParamsServlet extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        try
+        {
+            String firstName = "jeong";
+            String secondName = "jiwoon";
+            PrintWriter printWriter = response.getWriter();
+            // String contents = "Hello, World!";
+            String contents = "<!DOCTYPE html>\r\n" + //
+                    "<html lang=\"en\">\r\n" + //
+                    "<head>\r\n" + //
+                    "    <meta charset=\"UTF-8\">\r\n" + //
+                    "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + //
+                    "    <title> Hello world ! </title>\r\n" + //
+                    "</head>\r\n" + //
+                    "\r\n" + //
+                    "<body>\r\n" + //
+                    "    <div> first Name: " + firstName + " </div>\r\n" + //
+                    "    <div> second Name: " + secondName + "</div>\r\n" + //
+                    "    \r\n" + //
+                    "</body>\r\n" + //
+                    "</html>";
+            printWriter.println(contents);
+            printWriter.close();
+            // PrintWriter printWriter = response.getWriter();
+            // String contents = "";
+            // printWriter.println(contents);
+            // printWriter.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("종료");
+    }
+}

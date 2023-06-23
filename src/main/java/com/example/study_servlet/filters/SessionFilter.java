d14c7384b6653b2ee02a5578ab84edf7a06c506d
jeongjiwooon <jjwanm@naver.com>
2023-06-23 18:16:44 +0900

    commit

--- src/main/java/com/example/study_servlet/filters/SessionFilter.java
+++ src/main/java/com/example/study_servlet/filters/SessionFilter.java
@@ -4,12 +4,13 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
-@WebFilter(urlPatterns = "/*")
+// @WebFilter(urlPatterns = "/*")
 public class SessionFilter implements Filter
 {
 
@@ -20,6 +21,9 @@ public class SessionFilter implements Filter
         {
             System.out.println(request.getRemoteHost());
             System.out.println(request.getRemoteAddr());
+
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/helloWorldServlet"); // 클라이언트로부터 들어온 요청을 JSP/Servlet 내에서 원하는 자원으로 요청을 넘기는 역할
+            requestDispatcher.forward(request, response);
             chain.doFilter(request, response);
         }
 

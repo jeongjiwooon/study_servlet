5325c355a6f01fc1112bc2ae768c491e7de63eec
Your Name <you@example.com>
2023-06-26 10:53:24 +0900

    김민정B 수업내용

--- src/main/java/com/example/study_servlet/filters/SessionFilter.java
+++ src/main/java/com/example/study_servlet/filters/SessionFilter.java
@@ -4,29 +4,28 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
-@WebFilter(urlPatterns = "/*")
-public class SessionFilter implements Filter
-{
+// @WebFilter(urlPatterns = "/*")
+public class SessionFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)throws IOException, ServletException
-    {
-        try
-        {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        try {
             System.out.println(request.getRemoteHost());
             System.out.println(request.getRemoteAddr());
-            chain.doFilter(request, response);
-        }
 
-        catch (Exception e)
-        {
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/helloWorldServlet");
+            requestDispatcher.forward(request, response);
+
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
-    
+
 }
